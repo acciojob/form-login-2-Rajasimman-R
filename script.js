fa6df88f3342a94810bee54aab20e3c2c2307627
Rajasimman-R <131901523+Rajasimman-R@users.noreply.github.com>
2024-01-13 12:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ function submitForm() {
+            // Get values from input fields
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var email = document.getElementById("email").value;
+
+            // Display the result using alert with specific formatting
+            var resultMessage = "First Name: " + firstName + "\n"
+                              + "Last Name: " + lastName + "\n"
+                              + "Phone Number: " + phoneNumber + "\n"
+                              + "Email ID: " + email;
+
+            alert(resultMessage);}
